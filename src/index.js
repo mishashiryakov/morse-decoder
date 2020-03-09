add5124091e8baaf6ace1aa2f9d1f6661d3eac90
mishashiryakov <mishashiryakov@gmail.com>
2020-03-09 15:35:01 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+       
+    let arr = [];
+        let res = '';
+    let resArr = [];
+
+    for(let i = 0; i < expr.length; i += 10) {      //создаем массив с шифрами по 10 символов
+        arr.push(expr.slice(0 + i, 10 + i));
+    }
+
+    for(let i = 0; i < arr.length; i++) {       //создаем массив с ключами морза 
+    
+        for(let j = 0; j <= arr[i].length; j += 2) {
+            if (arr[i].slice(0 + j, 2 + j) == 11) {res += '-'};
+            if (arr[i].slice(0 + j, 2 + j) == 10) {res += '.'};
+            if (arr[i] == '**********') {res += ' '; break;};
+        }
+        resArr.push(res);
+        res = '';
+    }
+
+
+    let finRes = '';
+
+    for(let i = 0; i < resArr.length; i++) {            //преобразуем ключи морза в символы из объекта
+        if(resArr[i] == ' ') {finRes += ' '; continue};
+        finRes += MORSE_TABLE[resArr[i]];
+    } 
+
+    return finRes;
+
 }
 
 module.exports = {
